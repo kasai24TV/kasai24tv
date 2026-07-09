899446b5fbc623bdddb50b9c9b6a3d8cbf171d6d
jacquestshilonda3 <jacquestshilonda3@gmail.com>
2026-07-09 19:56:54 +0200

    Add Video model

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Video(models.Model):
+    titre = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    video_url = models.URLField()
+    publie = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titre
